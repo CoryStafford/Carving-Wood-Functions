9af7a73c77cb91b59adae88aa23427ac63c29031
CoryStafford <corystaffordmusic@gmail.com>
2022-02-18 15:37:22 -0600

    making math functions

---
+++ main.js
@@ -0,0 +1,16 @@
+const addValues = (num1, num2) => {
+    const math = num1 + num2 
+    return math
+}
+const addSum = addValues(2, 3)
+console.log(addSum)
+
+const multiplyValues = (num1) => {
+    const math = num1 * 2
+    return math
+}
+const multiplySum = multiplyValues(5)
+console.log(multiplySum)
+
+addValues(4)
+console.log(addValues)
